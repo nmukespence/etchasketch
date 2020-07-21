ae98e138c16a87d491115cf2a28c8f24a8752e39
Kenneth Spencer <nmukespence@gmail.com>
2020-07-21 11:52:03 -0700

    created functions to create grid and clean grid

---
+++ script.js
@@ -0,0 +1,43 @@
+
+
+const newButton = document.querySelector('#new');
+const cleanButton = document.querySelector('#clean');
+let selection = prompt("How Large Would You Like the Etch-A-Sketch?");
+const container = document.querySelector('#container');
+container.style.gridTemplateColumns = `repeat(${selection}, 1fr)`;
+
+
+create();
+onHover();
+
+cleanButton.addEventListener('click', function(){
+    hover.forEach(function(elem) {
+        elem.style.backgroundColor = 'white';
+    });
+})
+
+newButton.addEventListener('click', function(){
+    window.location.reload();
+});
+
+
+function create(){
+    selection = selection * selection;
+    for(i = 0; i < selection; i++){
+        let div = document.createElement('div');
+        div.className = 'grid';
+        document.querySelector("#container").appendChild(div);
+    }
+    window.hover = document.querySelectorAll('.grid');
+}
+
+function onHover(){
+    hover.forEach(function(elem) {
+        elem.addEventListener('mouseenter', function() {
+            elem.style.backgroundColor = 'black';
+        });
+    });
+}
+
+
+
